278ae5c79c88d4fb1a5f9790a4530f85420b9ecc
hayden <hayden321@gmail.com>
2021-01-07 11:16:01 -0800

    reformats style

--- src/main/scala/example/t.scala
+++ src/main/scala/example/t.scala
@@ -1,5 +1,5 @@
 object t {
- val usage = """
+  val usage = """
     Usage: mmlaln [--min-size num] [--max-size num] filename
   """
   def main(args: Array[String]) {
@@ -7,22 +7,25 @@ object t {
     val arglist = args.toList
     type OptionMap = Map[Symbol, Any]
 
-    def nextOption(map : OptionMap, list: List[String]) : OptionMap = {
-      def isSwitch(s : String) = (s(0) == '-')
+    def nextOption(map: OptionMap, list: List[String]): OptionMap = {
+      def isSwitch(s: String) = (s(0) == '-')
       list match {
         case Nil => map
-        case "--max-size" :: value :: tail =>
-                               nextOption(map ++ Map('maxsize -> value.toInt), tail)
+        case "--max-size" :: value :: tail => {
+          nextOption(map ++ Map('maxsize -> value.toInt), tail)
+        }
         case "--min-size" :: value :: tail =>
-                               nextOption(map ++ Map('minsize -> value.toInt), tail)
-        case string :: opt2 :: tail if isSwitch(opt2) => 
-                               nextOption(map ++ Map('infile -> string), list.tail)
-        case string :: Nil =>  nextOption(map ++ Map('infile -> string), list.tail)
-        case option :: tail => println("Unknown option "+option) 
-                               sys.exit(1) 
+          nextOption(map ++ Map('minsize -> value.toInt), tail)
+        case string :: opt2 :: tail if isSwitch(opt2) =>
+          nextOption(map ++ Map('infile -> string), list.tail)
+        case string :: Nil =>
+          nextOption(map ++ Map('infile -> string), list.tail)
+        case option :: tail =>
+          println("Unknown option " + option)
+          sys.exit(1)
       }
     }
-    val options = nextOption(Map(),arglist)
+    val options = nextOption(Map(), arglist)
     println(options)
-  } 
+  }
 }
